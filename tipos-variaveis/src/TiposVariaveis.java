aa6e726125491f3d83080c6ab7c8d05c026799bd
douglas980 <douglas_camargo980@outlook.com>
2026-02-25 18:06:54 -0300

    adiciona variáveis de exemplo no método main

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 25;
+        int populacao = 1500000;
+        long populacaoMundial = 8000000000L;
+        float preco = 10.99f;
+        double pi = 3.14159;
+        char inicial = 'J';
+        boolean ativo = true;
+        String nome = "Douglas";
+
     }
 }
